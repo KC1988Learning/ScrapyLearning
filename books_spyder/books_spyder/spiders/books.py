1b6199e65e3c0ff3dfbbccd1ca26bb6424340ae5
CaseyCheng1988 <60434600+CaseyCheng1988@users.noreply.github.com>
2020-11-27 18:18:57 +0800

    After updating books

--- books_spyder/books_spyder/spiders/books.py
+++ books_spyder/books_spyder/spiders/books.py
@@ -1,11 +1,13 @@
 # import scrapy
 # from scrapy.spiders import CrawlSpider, Rule
 # from scrapy.linkextractors import LinkExtractor
+from time import sleep
 
 from scrapy import Spider
 from selenium import webdriver
 from scrapy.selector import Selector
 from scrapy.http import Request
+from selenium.common.exceptions import NoSuchElementException
 
 class BooksSpider(Spider):
     name = 'books'
@@ -24,6 +26,7 @@ class BooksSpider(Spider):
     #     yield {'URL': response.url}
 
     def start_requests(self):
+        # enter each book link on the first page, parse and scrape
         self.driver = webdriver.Chrome(r"C:\Users\KC Cheng\Documents\chromedriver.exe")
         self.driver.get('http://books.toscrape.com')
 
@@ -32,6 +35,33 @@ class BooksSpider(Spider):
         for book in books:
             url = 'http://books.toscrape.com/' + book
             yield Request(url, callback=self.parse_book)
+
+        # Use Selenium to go to next page
+        while True:
+            try:
+                # next_page is the web element containing the text "next"
+                next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
+
+                # sleep for 3 s in case error arises in the request
+                # while Selenium tries to click the next page again
+                sleep(3);
+                self.log("Sleeping for 3 seconds.")
+
+                # click on the next page button
+                next_page.click()
+
+               # once the new page is accessed, select the page source again
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parse_book)
+
+            # Break the loop when reaching the last page
+            except NoSuchElementException:
+                self.log.info("No more pages to load.")
+                self.driver.quit() # quite the drive instance
+                break
 
     def parse_book(self, response):
         pass
